dd5dedd2b6f676761c9ebee146baab9b22b0079c
Alex Nicoll <code.alexn@gmail.com>
2023-07-10 00:52:34 +0000

    Refactor handler and switch to json.Unmarshal
    - Refactor API handler into multiple methods
    - Slightly reduce scope of version, todos, and mu
    - Switch from using json.Decoder to json.Unmarshal in preparation for
      implementing append. We keep the whole request body in memory to
      facilitate matching against different request types.

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"encoding/json"
+	"io"
 	"log"
 	"math"
 	"net/http"
@@ -10,15 +11,73 @@ import (
 	"golang.org/x/exp/slices"
 )
 
+func main() {
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		http.ServeFile(w, r, "./main.html")
+	})
+	http.HandleFunc("/main.js", func(w http.ResponseWriter, r *http.Request) {
+		http.ServeFile(w, r, "./main.js")
+	})
+	http.Handle("/api", &apiHandler{
+		version: 0,
+		todos: [][2]string{
+			[2]string{"a", "todo A"},
+			[2]string{"b", "todo B"},
+			[2]string{"c", "todo C"},
+			[2]string{"d", "todo D"},
+		},
+		mu: &sync.Mutex{},
+	})
+	http.ListenAndServe(":8080", nil)
+}
+
+type apiHandler struct {
+	version uint32
+	todos   [][2]string
+	mu      *sync.Mutex
+}
+
+func (h *apiHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	if r.Method == http.MethodGet {
+		h.serveGet(w)
+	}
+	if r.Method == http.MethodPost {
+		body, err := io.ReadAll(r.Body)
+		if err != nil {
+			log.Println(err)
+			w.WriteHeader(http.StatusBadRequest)
+			return
+		}
+		dr := &deleteRqst{}
+		if err := json.Unmarshal(body, dr); err == nil && dr.Operation == "delete" {
+			h.serveDelete(w, dr)
+			return
+		}
+		log.Printf("received unrecognized JSON: %s", body)
+		w.WriteHeader(http.StatusBadRequest)
+		return
+	}
+}
+
 // Fields must be exported in order for encoding/json to access them.
 
-type todos struct {
+type getResp struct {
 	Version uint32      `json:"version"`
 	Todos   [][2]string `json:"todos"`
 }
 
+func (h *apiHandler) serveGet(w http.ResponseWriter) {
+	h.mu.Lock()
+	writeJson(w, &getResp{
+		Version: h.version,
+		Todos:   h.todos,
+	})
+	h.mu.Unlock()
+	return
+}
+
 type deleteRqst struct {
-	Operation string // must be "delete"
+	Operation string
 	Version   uint32
 	Key       string
 }
@@ -32,81 +91,41 @@ type deleteMismatchResp struct {
 	Todos   [][2]string `json:"todos"`
 }
 
-func main() {
-	t := &todos{
-		Version: 0,
-		Todos: [][2]string{
-			[2]string{"a", "todo A"},
-			[2]string{"b", "todo B"},
-			[2]string{"c", "todo C"},
-			[2]string{"d", "todo D"},
-		},
-	}
-	mu := &sync.Mutex{}
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		http.ServeFile(w, r, "./main.html")
-	})
-	http.HandleFunc("/main.js", func(w http.ResponseWriter, r *http.Request) {
-		http.ServeFile(w, r, "./main.js")
-	})
-	http.HandleFunc("/api", func(w http.ResponseWriter, r *http.Request) {
-		if r.Method == http.MethodGet {
-			mu.Lock()
-			writeJson(w, t)
-			mu.Unlock()
-			return
+func (h *apiHandler) serveDelete(w http.ResponseWriter, r *deleteRqst) {
+	h.mu.Lock()
+	defer h.mu.Unlock()
+	todoIndex := -1
+	for i, todo := range h.todos {
+		if todo[0] == r.Key {
+			todoIndex = i
+			break
 		}
-		if r.Method == http.MethodPost {
-			dec := json.NewDecoder(r.Body)
-			dec.DisallowUnknownFields()
-			rqst := &deleteRqst{}
-			err := dec.Decode(rqst)
-			if err == nil {
-				if rqst.Operation != "delete" {
-					w.WriteHeader(http.StatusBadRequest)
-					return
-				}
-				mu.Lock()
-				defer mu.Unlock()
-				todoIndex := -1
-				for i, todo := range t.Todos {
-					if todo[0] == rqst.Key {
-						todoIndex = i
-						break
-					}
-				}
-				if rqst.Version == t.Version {
-					if todoIndex == -1 {
-						w.WriteHeader(http.StatusBadRequest)
-						return
-					}
-					deleteTodo(t, todoIndex)
-					writeJson(w, &deleteResp{Version: t.Version})
-					return
-				}
-				// version mismatch
-				log.Println("version mismatch")
-				if todoIndex != -1 {
-					deleteTodo(t, todoIndex)
-				}
-				writeJson(w, &deleteMismatchResp{
-					Version: t.Version,
-					Todos:   t.Todos,
-				})
-				return
-			}
-			log.Println(err)
+	}
+	if r.Version == h.version {
+		if todoIndex == -1 {
 			w.WriteHeader(http.StatusBadRequest)
 			return
 		}
+		h.deleteTodo(todoIndex)
+		writeJson(w, &deleteResp{Version: h.version})
+		return
+	}
+	// version mismatch
+	log.Println("version mismatch")
+	if todoIndex != -1 {
+		h.deleteTodo(todoIndex)
+	}
+	writeJson(w, &deleteMismatchResp{
+		Version: h.version,
+		Todos:   h.todos,
 	})
-	http.ListenAndServe(":8080", nil)
+	return
 }
 
-func deleteTodo(t *todos, todoIndex int) {
-	t.Todos = slices.Delete(t.Todos, todoIndex, todoIndex+1)
-	incrementVersion(&(t.Version))
-	log.Println(t)
+func (h *apiHandler) deleteTodo(todoIndex int) {
+	h.todos = slices.Delete(h.todos, todoIndex, todoIndex+1)
+	incrementVersion(&(h.version))
+	log.Println(h.version, h.todos)
 }
 
 func incrementVersion(v *uint32) {
