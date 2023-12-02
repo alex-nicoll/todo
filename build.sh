7e111b51a4a20aa672b8992fbeda9e46b0cf6c25
Alex Nicoll <code.alexn@gmail.com>
2023-12-02 21:16:00 +0000

    Fix faulty CI on non-main branches

--- build.sh
+++ build.sh
@@ -5,4 +5,4 @@ docker build . --target ts-check &&
 docker build . --target ts-lint &&
 # Uncomment this line after lint findings are addressed.
 #docker build . --target go-lint &&
-docker build . --target bin -t $1
+docker build . --target bin $1
--- run.sh
+++ run.sh
@@ -5,7 +5,7 @@ TAG=$1
 if [ -z $TAG ]; then
   TAG='todo:latest'
 fi
-"$SCRIPT_PATH"/build.sh $TAG &&
+"$SCRIPT_PATH"/build.sh "-t $TAG" &&
 docker run -it --rm \
   -p 8080:8080 \
   -e "JWT_SIGNING_KEY=$JWT_SIGNING_KEY" \
